ca5ec6dcf18bf5cfea44784dd68e948f0ce4638f
Adriancito303 <70454480+Adriancito303@users.noreply.github.com>
2024-03-15 20:26:46 -0600

    Agregar archivos de proyecto.

---
+++ WinFormsJenkins_CesarSilva/Form1.cs
@@ -0,0 +1,15 @@
+namespace WinFormsJenkins_CesarSilva
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola Mundo, César Silva, 1184519");
+        }
+    }
+}
